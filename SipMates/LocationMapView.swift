d377a06cc176cf3d077b05bbc3ed8304afc57a4b
nhi-ngo <nhingo.y@gmail.com>
2024-08-20 12:35:49 -0400

    show the map on MapView

--- SipMates/LocationMapView.swift
+++ SipMates/LocationMapView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     var body: some View {
-        Text("Location Map View")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+        }
     }
 }
 
