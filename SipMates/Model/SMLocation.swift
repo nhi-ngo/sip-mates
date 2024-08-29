93f9484327807be7f6d12b2b2f6aab03e1cf8e85
nhi-ngo <nhingo.y@gmail.com>
2024-08-29 11:37:30 -0400

    create  model object for SMLocation

---
+++ SipMates/Model/SMLocation.swift
@@ -0,0 +1,41 @@
+//
+//  SMLocation.swift
+//  SipMates
+//
+//  Created by Nhi Ngo on 8/29/24.
+//
+
+import CloudKit
+
+struct SMLocation {
+    static let kName = "name"
+    static let kDescription = "description"
+    static let kSquareAsset = "squareAsset"
+    static let kBannerAsset = "bannerAsset"
+    static let kAddress = "address"
+    static let kLocation = "location"
+    static let kWebsiteURL = "websiteURL"
+    static let kPhoneNumber = "phoneNumber"
+    
+    let ckRecordId: CKRecord.ID
+    let name: String
+    let description: String
+    let squareAsset: CKAsset!
+    let bannerAsset: CKAsset!
+    let address: String
+    let location: CLLocation
+    let websiteURL: String
+    let phoneNumber: String
+    
+    init(record: CKRecord) {
+        ckRecordId  = record.recordID
+        name         = record[SMLocation.kName] as? String ?? "N/A"
+        description = record[SMLocation.kDescription] as? String ?? "N/A"
+        squareAsset = record[SMLocation.kSquareAsset] as? CKAsset
+        bannerAsset = record[SMLocation.kBannerAsset] as? CKAsset
+        address     = record[SMLocation.kAddress] as? String ?? "N/A"
+        location    = record[SMLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+        websiteURL  = record[SMLocation.kWebsiteURL] as? String ?? "N/A"
+        phoneNumber = record[SMLocation.kPhoneNumber] as? String ?? "N/A"
+    }
+}
