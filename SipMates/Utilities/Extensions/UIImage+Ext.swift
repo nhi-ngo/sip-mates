59e7d2a0e2db3893ca5249b12862478f4bfb4a05
nhi-ngo <nhingo.y@gmail.com>
2024-09-15 09:19:48 +0700

    convert UIImage to CKAsset

---
+++ SipMates/Utilities/Extensions/UIImage+Ext.swift
@@ -0,0 +1,34 @@
+//
+//  UIImage+Ext.swift
+//  SipMates
+//
+//  Created by Nhi Ngo on 9/14/24.
+//
+
+import CloudKit
+import UIKit
+
+extension UIImage {
+    
+    func convertToCKAsset() -> CKAsset? {
+        // get our apps base document directory url
+        guard let urlPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Document directory url came back nil")
+            return nil
+        }
+        
+        // append unique identifier for profile image
+        let fileUrl = urlPath.appendingPathComponent("selectedAvatar")
+        
+        // write the image data to the location the address points to
+        let imageData = jpegData(compressionQuality: 0.25)
+        
+        // create our CKAsset with that fileURL
+        do {
+            try imageData?.write(to: fileUrl)
+            return CKAsset(fileURL: fileUrl)
+        } catch {
+            return nil
+        }
+    }
+}
